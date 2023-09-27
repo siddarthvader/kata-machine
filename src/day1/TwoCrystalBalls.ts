55cf4975d5b98ca8cdd08ca3a87bc369ec2fc5cf
siddharth <siddharth@godspeed.systems>
2023-09-27 12:18:54 +0530

    crysal ball problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let firstBall = jumpAmount;
 
+    for (; firstBall < breaks.length; firstBall += jumpAmount) {
+        if (breaks[firstBall]) {
+            break;
+        }
+    }
+
+    firstBall = firstBall - Math.floor(Math.sqrt(breaks.length));
+
+    for (
+        let secondBall = 0;
+        secondBall < jumpAmount && firstBall < breaks.length;
+        secondBall++, firstBall++
+    ) {
+        if (breaks[firstBall]) {
+            return firstBall;
+        }
+    }
+    return -1;
 }
